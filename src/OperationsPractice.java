77dc571085845cf3744d84df347344b2e7d38e70
sravan parakala <43002608+sparakala21@users.noreply.github.com>
2018-10-01 08:32:36 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,20 @@
+
+public class OperationsPractice {
+
+	public static void main(String[] args)
+	{
+		int five = 9;
+		five += 5;
+		System.out.println("add five "+five); //Stupid computer says 5=7
+		five /= 2;
+		System.out.println("divide by two "+five); //Stupid computer says 5=7
+		five*=4;
+		System.out.println("multiply by two "+five); //Stupid computer says 5=7
+		five-=6;
+		System.out.println("subtract six "+five); //Stupid computer says 5=7
+		five%=2;
+		System.out.println("modularly divide by two "+five); //Stupid computer says 5=7
+		
+	}
+
+}
